e3ede727f1e6005423f0d9d92749ad928deabd25
myosubug <seanoh.dev@gmail.com>
2019-04-06 22:59:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# to-do list
+
+This is a simple to-do list that can add/remove to-dos.
+
+## Live example
+
+* 
+
+### Prerequisites
+
+* any web browser
+
+### Installing
+
+No installation is required.
+
+## Built With
+
+* Vanilla JS
+* html
+* css
+
+
---
+++ todo.js
@@ -0,0 +1,26 @@
+//check off speific to-dos
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+//click x to delte to-do
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        //grabbing new todo text
+        var todotext = $(this).val();
+        $(this).val("");
+        //create new li and add it to ul
+        $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todotext + "</li>");
+    }
+});
+
+$(".fa-plus-circle").click(function() {
+    $("input[type='text']").fadeToggle();
+});
